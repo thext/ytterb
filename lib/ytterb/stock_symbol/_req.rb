736f9293b921fd226e8f3e3d4f8ae34dfd3379d0
thext <thext@outlook.com>
2014-06-01 22:30:51 -0700

    remove debug puts from _req

--- lib/ytterb/stock_symbol/_req.rb
+++ lib/ytterb/stock_symbol/_req.rb
@@ -1,5 +1,2 @@
 path = File.expand_path(File.dirname(__FILE__))
-Dir.glob(File.join(path,"**","*.rb")).each do |file|
-  puts "require #{file}"
-  require file
-end
+Dir.glob(File.join(path,"**","*.rb")).each {|f| require f }
