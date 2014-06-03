936ec40dc50131a70726dae67a0a21581f5f14fe
thext <thext@outlook.com>
2014-06-03 00:15:14 -0700

    add nuke script to cleanup cached_symbol_data completely enhance clean script to output operation that it performs

--- lib/ytterb/.cache/cached_symbol_data/nuke.sh
+++ lib/ytterb/.cache/cached_symbol_data/nuke.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Cleaning $DIR... ((remove all empty yaml stock cache files))"
+echo "Nuking $DIR... (remove all files yaml stock cache files)"
 
+find $DIR -print | grep yaml$ | xargs truncate -s 0
 find $DIR -empty -type f -delete
 find $DIR -empty -type d -delete
